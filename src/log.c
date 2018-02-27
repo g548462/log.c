9b2128866ecaf12b22beb871602ba2e2de189271
g548462 <g548462@il.com>
2018-02-27 15:50:40 +0800

    printf func_name instead of filename

--- src/log.c
+++ src/log.c
@@ -89,7 +89,7 @@ void log_set_quiet(int enable) {
 }
 
 
-void log_log(int level, const char *file, int line, const char *fmt, ...)
+void log_log(int level, const char *func, int line, const char *fmt, ...)
 {
     if (level < L.level) {
         return;
@@ -109,9 +109,9 @@ void log_log(int level, const char *file, int line, const char *fmt, ...)
         buf[strftime(buf, sizeof(buf), "%H:%M:%S", lt)] = '\0';
 #ifdef LOG_USE_COLOR
         fprintf(stderr, "%s %s%-5s\x1b[0m \x1b[90m%s:%d:\x1b[0m ", buf, level_colors[level],
-                level_names[level], file, line);
+                level_names[level], func, line);
 #else
-        fprintf(stderr, "%s %-5s %s:%d: ", buf, level_names[level], file, line);
+        fprintf(stderr, "%s %-5s %s:%d: ", buf, level_names[level], func, line);
 #endif
         va_start(args, fmt);
         vfprintf(stderr, fmt, args);
@@ -124,7 +124,7 @@ void log_log(int level, const char *file, int line, const char *fmt, ...)
         va_list args;
         char buf[32];
         buf[strftime(buf, sizeof(buf), "%Y-%m-%d %H:%M:%S", lt)] = '\0';
-        fprintf(L.fp, "%s %-5s %s:%d: ", buf, level_names[level], file, line);
+        fprintf(L.fp, "%s %-5s %s:%d: ", buf, level_names[level], func, line);
         va_start(args, fmt);
         vfprintf(L.fp, fmt, args);
         va_end(args);
--- src/log.h
+++ src/log.h
@@ -23,12 +23,12 @@ enum {
     LOGFATAL
 };
 
-#define log_trace(...) log_log(LOGTRACE, __FILE__, __LINE__, __VA_ARGS__)
-#define log_debug(...) log_log(LOGDEBUG, __FILE__, __LINE__, __VA_ARGS__)
-#define log_info(...)  log_log(LOGINFO,  __FILE__, __LINE__, __VA_ARGS__)
-#define log_warn(...)  log_log(LOGWARN,  __FILE__, __LINE__, __VA_ARGS__)
-#define log_error(...) log_log(LOGERROR, __FILE__, __LINE__, __VA_ARGS__)
-#define log_fatal(...) log_log(LOGFATAL, __FILE__, __LINE__, __VA_ARGS__)
+#define log_trace(...) log_log(LOGTRACE, __func__, __LINE__, __VA_ARGS__)
+#define log_debug(...) log_log(LOGDEBUG, __func__, __LINE__, __VA_ARGS__)
+#define log_info(...)  log_log(LOGINFO,  __func__, __LINE__, __VA_ARGS__)
+#define log_warn(...)  log_log(LOGWARN,  __func__, __LINE__, __VA_ARGS__)
+#define log_error(...) log_log(LOGERROR, __func__, __LINE__, __VA_ARGS__)
+#define log_fatal(...) log_log(LOGFATAL, __func__, __LINE__, __VA_ARGS__)
 
 void log_set_udata(void *udata);
 void log_set_lock(log_LockFn fn);
@@ -36,6 +36,6 @@ void log_set_fp(FILE *fp);
 void log_set_level(int level);
 void log_set_quiet(int enable);
 
-void log_log(int level, const char *file, int line, const char *fmt, ...);
+void log_log(int level, const char *func, int line, const char *fmt, ...);
 
 #endif
